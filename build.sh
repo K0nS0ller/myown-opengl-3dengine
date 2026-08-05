7f8c0658f693e78fc27219824c7e32a70f92be1f
k0nnsolerr <203224951+K0nS0ller@users.noreply.github.com>
2026-08-05 10:48:10 +0300

    Enhance build.sh with better file handling and output
    Refactor build script to improve readability and structure.

--- build.sh
+++ build.sh
@@ -1,3 +1,88 @@
 #!/bin/bash
 
-cd content; find . -type f -name '*.h' -print -delete; ./pack.sh; cd ..; gcc main.c include/glad.c -o engine -lGL -lglfw -lm -flto -Os -funroll-loops -fexpensive-optimizations -pipe -s
+cd content; find . -type f -name '*.h' -print -delete
+
+set -e
+
+files=$(find . -type f \
+    ! -name "*.h" \
+    ! -name "*.c" \
+    ! -name "*.sh" \
+    ! -name "*.png")
+
+total=$(echo "$files" | wc -l)
+current=0
+
+for file in $files
+do
+    current=$((current + 1))
+
+    out="${file%.*}.h"
+
+    name="$(
+        echo "$file" |
+        sed 's|^\./||' |
+        tr '/.-' '_'
+    )"
+
+    echo "packing $file to $out ($current/$total)"
+
+    xxd -i -n "$name" "$file" > "$out"
+done
+
+
+images=$(find . -name "*.png")
+
+img_total=$(echo "$images" | wc -l)
+img_current=0
+
+for img in $images
+do
+    img_current=$((img_current + 1))
+
+    raw="${img%.png}.raw"
+    header="${img%.png}.h"
+
+    echo "converting $img ($img_current/$img_total)"
+
+    convert "$img" RGBA:"$raw"
+
+    name="$(
+        echo "$raw" |
+        sed 's|^\./||' |
+        tr '/.-' '_'
+    )"
+
+    xxd -i -n "$name" "$raw" > "$header"
+
+    rm "$raw"
+done
+
+images=$(find . -name "*.jpg")
+
+img_total=$(echo "$images" | wc -l)
+img_current=0
+
+for img in $images
+do
+    img_current=$((img_current + 1))
+
+    raw="${img%.jpg}.raw"
+    header="${img%.jpg}.h"
+
+    echo "converting $img ($img_current/$img_total)"
+
+    convert "$img" RGBA:"$raw"
+
+    name="$(
+        echo "$raw" |
+        sed 's|^\./||' |
+        tr '/.-' '_'
+    )"
+
+    xxd -i -n "$name" "$raw" > "$header"
+
+    rm "$raw"
+done
+
+cd ..; gcc main.c include/glad.c -o engine -lGL -lglfw -lm -flto -Os -funroll-loops -fexpensive-optimizations -pipe -s
