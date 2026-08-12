400bd345a044d1efb458809224a7c87efc34493a
k0nS0ller <203224951+K0nS0ller@users.noreply.github.com>
2026-08-12 23:31:43 +0300

    Add files via upload

---
+++ game/resources.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//shaders
+#include "../content/shaders/vertex_shader.h"
+#include "../content/shaders/fragment_shader.h"
+#include "../content/shaders/vat_shader.h"
+
+//textures
+#include "../content/textures/grass.h"
+#include "../content/textures/coin.h"
+
+//models
+#include "../content/assets/terrain.h"
+#include "../content/assets/coin.h"
